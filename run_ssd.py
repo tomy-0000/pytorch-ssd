d414ace0729e377831cd97702336381121091a3e
tomy <yutotom0705@gmail.com>
2021-04-27 16:38:29 +0900

    gpuに対応

--- run_ssd.py
+++ run_ssd.py
@@ -2,9 +2,11 @@ from .vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, creat
 import cv2
 import sys
 
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 net = create_mobilenetv2_ssd_lite(2, is_test=True)
 net.load("path to pth")
-predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
+net.to(DEVICE)
+predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200, device=DEVICE)
 
 def ssd(orig_image):
     image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
